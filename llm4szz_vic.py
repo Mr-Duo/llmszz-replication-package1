3a2deffbe9a27fd602b0631b9c430162b16f8e4f
Trần Sỹ Minh Quân <131475958+TheSyx04@users.noreply.github.com>
2025-11-09 06:59:32 +0700

    Update llm4szz_vic.py

--- llm4szz_vic.py
+++ llm4szz_vic.py
@@ -13,7 +13,7 @@ from prompts import *
 from util import *
 
 # Configuration - Update this to point to your VFC dataset
-VFC_DATASET_FILE = "vfc_dataset.json"  # File created by convert_vfc_to_dataset.py
+VFC_DATASET_FILE = "vfc_using_msg_regex.json"  # File created by convert_vfc_to_dataset.py
 
 # Load VFC data
 all_info = []
