4d7a78fddcb09db97ae1aaadd90faf2c309cd567
Trần Sỹ Minh Quân <131475958+TheSyx04@users.noreply.github.com>
2025-11-09 07:10:13 +0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -5,7 +5,7 @@ CWD = "/data/scratch/projects/punim1928/HUST/thesyx/llmszz-replication-package1"
 # enter your directory which saves all repos
 REPOS_DIR = "/data/scratch/projects/punim1928/HUST/thesyx"
 
-DATASET_DIR = os.path.join(CWD,'dataset/VFC')
+DATASET_DIR = os.path.join(CWD,'dataset')
 SAVE_LOG_DIR = os.path.join(CWD,'save_logs')
 TMP_DIR = os.path.join(CWD,'tmp_dir') 
 TREE_SITTER_DIR = os.path.join(CWD,'build')
